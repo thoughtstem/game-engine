9b452cda16bc74736169707d408ab9c43efa5e80
antwonpark <atp034@ucsd.edu>
2018-04-16 16:29:25 -0700

    delete

--- main.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-(provide (all-from-out "./game-engine.rkt")
-         ;(all-from-out "./spaceship-game/main.rkt")
-         )
-
-(require "./game-engine.rkt")
-;(require "./spaceship-game/main.rkt")
