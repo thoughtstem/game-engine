469989347da73bb12765b2e1d562c5fefa7f95e4
antwonpark <atp034@ucsd.edu>
2018-04-30 12:36:13 -0700

    Updated after-time
    Updated after-time to delete itself after it runs

--- components/after-time.rkt
+++ components/after-time.rkt
@@ -10,10 +10,6 @@
 (define (make-after-time ticks func)
   (after-time 0 ticks func))
 
-(define (reset-after-time a)
-  (struct-copy after-time a
-               [accum 0]))
-
 (define (inc-after-time a)
   (struct-copy after-time a
                [accum (add1 (after-time-accum a))]))
@@ -24,9 +20,8 @@
 
 (define (update-after-time g e c)
   (if (after-time-ready? c)
-      (update-entity ((after-time-func c) g e) after-time? reset-after-time)
-      (update-entity e                     after-time? inc-after-time)))
+      (remove-component ((after-time-func c) g e) after-time?)
+      (update-entity    e                         after-time? inc-after-time)))
 
 (new-component after-time?
                update-after-time)
-
