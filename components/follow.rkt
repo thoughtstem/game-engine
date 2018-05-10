34ad71973ed078cec0655edb1f43a1c6ae7c9f8f
aBlender <jasonlongle@gmail.com>
2018-05-10 13:04:07 -0700

    Added Follow Component

---
+++ components/follow.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(require "../game-entities.rkt")
+(require "./direction.rkt")
+(require posn)
+
+(provide (rename-out (make-follow follow)))
+
+;(provide (struct-out do-every))
+
+(struct follow (accum name speed))
+
+(define (make-follow name [ticks 1])
+  (follow 0 name ticks))
+
+(define (reset-follow a)
+  (struct-copy follow a
+               [accum 0]))
+
+(define (inc-follow a)
+  (struct-copy follow a
+               [accum (add1 (follow-accum a))]))
+
+(define (follow-ready? a)
+  (>= (follow-accum a)
+      (follow-speed a)))
+
+(define (update-follow g e c)
+  (define target? (get-entity (follow-name c) g))
+  (define target-x (unless (eq? target? #f) (posn-x (get-component target? posn?))))
+  (define target-y (unless (eq? target? #f) (posn-y (get-component target? posn?))))
+  (define x (posn-x (get-component e posn?)))
+  (define y (posn-y (get-component e posn?)))
+  (define new-dir (unless (eq? target? #f)(radians->degrees (atan (- target-y y) (- target-x x)))))
+  (define update-direction
+    (update-entity e direction? (direction (if (negative? new-dir)
+                                                 (+ 360 new-dir)
+                                                 new-dir))))
+  (cond
+    [(and (follow-ready? c) target?)
+     (update-entity update-direction follow? reset-follow)]
+    [(and (not (follow-ready? c)) target?)
+     (update-entity e                     follow? inc-follow)]
+    [else e]))
+
+(new-component follow?
+               update-follow)
+
