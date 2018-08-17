8f03b8af93891d8bd63ca309ab8165cbb47e040a
aBlender <jasonlongle@gmail.com>
2018-08-17 11:23:20 -0700

    Used modulo to fix out of range degrees

--- components/direction.rkt
+++ components/direction.rkt
@@ -16,7 +16,7 @@
 
 (define (set-direction d)
  (lambda (g e)
-     (update-entity e direction? (direction d))))
+     (update-entity e direction? (direction (modulo d 360)))))
 
 (define (get-direction e)
   (direction-dir (get-component e direction?)))
@@ -24,17 +24,17 @@
 (define (change-direction-by inc)
   (lambda (g e)
     (define d (get-direction e))
-    (update-entity e direction? (direction (+ d inc)))))
+    (update-entity e direction? (direction (modulo (+ d inc) 360)))))
 
 (define (random-direction min max)
   (lambda (g e)
-     (update-entity e direction? (direction (random min (add1 max))))))
+     (update-entity e direction? (direction (modulo (random min (add1 max)) 360)))))
 
 (define (bounce-back)
   (lambda (g e)
     (define dir (get-direction e))
     (define new-dir (+ dir 180))
-    (update-entity e direction? (direction new-dir))))
+    (update-entity e direction? (direction (modulo new-dir 360)))))
 
 (new-component direction?
                update-direction) 
