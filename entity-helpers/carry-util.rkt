ab5c6099d3b8fda6b437f5e0d499a8d3344865c7
aBlender <jasonlongle@gmail.com>
2018-09-21 13:36:24 -0700

    carry helper

---
+++ entity-helpers/carry-util.rkt
@@ -0,0 +1,66 @@
+#lang racket
+
+(provide movable
+         get-carry-offset
+         )
+
+(require "../game-entities.rkt"
+         "../components/backdrop.rkt"
+         "../components/lock-to.rkt"
+         "../components/active-on-bg.rkt"
+         "../entity-helpers/movement-util.rkt"
+         "../components/animated-sprite.rkt"
+         "../components/on-key.rkt"
+         "../component-util.rkt"
+         posn
+         2htdp/image
+         )
+
+; === GENENRIC RULES AND HELPERS ===
+; TODO: add to game engine
+(define (remove-active-on-bg)
+  (lambda (g e)
+    (remove-component e active-on-bg?)))
+
+(define (add-active-on-bg)
+  (lambda (g e)
+    (define current-tile (get-current-tile (get-entity "bg" g)))
+    (add-component e (active-on-bg current-tile))))
+
+(define (add-lock-to name #:offset [offset (posn 10 0)])
+  (lambda (g e)
+    (if (get-component e lock-to?)
+        e
+        (add-component (remove-component e physical-collider?) (lock-to name #:offset offset)))))
+
+(define (remove-lock-to)
+  (lambda (g e)
+    (add-component (remove-component e lock-to?) (physical-collider))))
+
+(define (near-player-and-not-carried? g e)
+    (and ((near-entity? "player") g e)
+         (not (get-component e lock-to?))))
+
+(define (carried? g e)
+  (get-component e lock-to?))
+
+(define (get-carry-offset player-sprite item-sprite)
+  (define p-img (render player-sprite))
+  (define i-img (render item-sprite))
+  (define pos-x (+ (/ (image-width p-img) 2) (/ (image-width i-img) 2)))
+  (define pos-y 0)
+  (posn pos-x pos-y))
+
+; === GENERIC SYSTEM ===
+; TODO: only carry on at a time option
+;       calculate offset from game entities, use struct?
+(define (movable #:carry-offset   [offset (posn 0 0)]
+                 #:storable-items [movable-item-list #f]
+                 #:pickup-key     [pickup-key "z"]
+                 #:drop-key       [drop-key "x"]
+                 #:pickup-sound   [pickup-sound #f]
+                 #:drop-sound     [drop-sound    #f])
+  (list (on-key pickup-key #:rule near-player-and-not-carried? (do-many (add-lock-to "player" #:offset offset)
+                                                                        (remove-active-on-bg)))
+        (on-key drop-key #:rule carried? (do-many (remove-lock-to)
+                                                  (add-active-on-bg)))))
--- game-engine.rkt
+++ game-engine.rkt
@@ -40,6 +40,7 @@
  (all-from-out "entity-helpers/sprite-util.rkt")
  (all-from-out "entity-helpers/rgb-hsb.rkt")
  (all-from-out "entity-helpers/dialog-util.rkt")
+ (all-from-out "entity-helpers/carry-util.rkt")
  (all-from-out "ai.rkt")
 
 
@@ -86,5 +87,6 @@
 (require "entity-helpers/sprite-util.rkt")
 (require "entity-helpers/rgb-hsb.rkt")
 (require "entity-helpers/dialog-util.rkt")
+(require "entity-helpers/carry-util.rkt")
 (require "ai.rkt")
 
