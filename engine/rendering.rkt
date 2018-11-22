19e04f467cd3177468751fbf773bd82da4c7199a
srfoster <stephen@thoughtstem.com>
2018-11-22 11:08:28 -0800

    factoring out mode-lambda stuff more

--- engine/rendering.rkt
+++ engine/rendering.rkt
@@ -19,6 +19,8 @@
 (require "./core.rkt")
 (require "../components/animated-sprite.rkt")
 
+
+
 (define (lux-start larger-state)
   (define render-tick (get-mode-lambda-render-tick (game-entities larger-state)))
 
@@ -26,7 +28,6 @@
   (call-with-chaos
    (make-gui #:start-fullscreen? #f #:mode gl:gui-mode)
    (λ () (fiat-lux (demo larger-state render-tick)))))
-
 
 
 
@@ -42,71 +43,39 @@
 
 
   ;Use the entities, plus their sprites, to determine the initial sprite database
-  (define csd (entities->compiled-sprite-database original-entities))
+  (set! csd (entities->compiled-sprite-database original-entities))
 
   ;Define that we'll have one layer of sprites (for now).
-  ;  Fix it's position at the width and height of the game (WHY NOT 0,0?)
+  ;Fix its position at the center of the screen
   (define layers (vector (ml:layer (real->double-flonum W/2)
                                    (real->double-flonum H/2))))
 
-  
+  ;Save our compiled entities, so we can update it later as new entities are spawned and need to be compiled
+  ;   NOTE:  Should we really be storing entities?  Probably makes more sense to store sprites instead...
+  (set! compiled (setup-already-compiled-list original-entities))
 
-  (define compiled original-entities)
-
-  (define ml:render (gl:stage-draw/dc csd W H 8))
+  ;Set up our open gl render function with the current sprite database
+  (set! ml:render (gl:stage-draw/dc csd W H 8))
   
-  (define (ticky-tick)
+  (define (ticky-tick current-entities)
     
     ;Find uncompiled entities...
-    (define uncompiled (if (not last-game-snapshot)
-                           '()
-                           (filter-not (curryr member compiled entity-eq? #;sprite-eq?)
-                                       (game-entities last-game-snapshot))))
-
+    (define uncompiled (find-uncompiled-things current-entities compiled))
 
     ;Recompile the database if we added anything:
     (and (not (empty? uncompiled))
-         (let ([sd2 (ml:make-sprite-db)])
-           (for ([sprite-entity (in-list (append compiled uncompiled))])
-             (and (get-component sprite-entity animated-sprite?)
-                  (add-entity! sd2 sprite-entity)))
-           (set! csd (ml:compile-sprite-db sd2))
-           ;(ml:save-csd! csd (build-path "/Users/thoughtstem/Desktop/sprite-db") #:debug? #t)
-           (set! compiled (append compiled uncompiled))
-           (displayln (ml:compiled-sprite-db-spr->idx csd))
-           (set! ml:render (gl:stage-draw/dc csd W H 8))))
+         (recompile uncompiled W H))
 
+    ;Create our sprites
+    (define dynamic-sprites
+      (if (not current-entities) ;First frame?  
+          '()                      ;No sprites
+          (game->mode-lambda-sprite-list current-entities)))
 
+    (define static-sprites '())
 
-    (define (sprites-to-render)
-      (if (not last-game-snapshot)
-          '()
-          (flatten
-           (filter identity
-                   (for/list ([e (in-list (reverse (game-entities last-game-snapshot)))])
-                     (define frame-i   (animated-sprite-current-frame (get-component e animated-sprite?)))
-                     (define id-sym    (id->symbol #:prefix (~a "sprite" frame-i) (get-id e)))
-                     (define sprite-id (ml:sprite-idx csd id-sym))
-
-                    ; (displayln (~a (get-name e) " id: " id-sym " sprite-id: " sprite-id))
-
-                     (if (not sprite-id)
-                           #f
-                           (ml:sprite #:layer 0
-                                      (real->double-flonum (x e))
-                                      (real->double-flonum (y e))
-                                      
-                                      #;(real->double-flonum (- (x e) W/2))
-                                      #;(real->double-flonum (- (y e) H/2))
-
-                                      
-                                      sprite-id)))))))
-
-    
-    
-    (ml:render layers
-               (sprites-to-render)
-               '()))
+    ;Actually render them
+    (ml:render layers dynamic-sprites static-sprites))
 
   ticky-tick)
 
@@ -124,7 +93,7 @@
    (define (word-output w)
      (match-define (demo  state render-tick) w)
      #;(g/v (draw state)) ;Old, slower drawing method.  For reference...
-     (render-tick)
+     (render-tick last-game-snapshot)
      )
    
    (define (word-event w e)
@@ -184,3 +153,49 @@
 
 
 
+
+
+
+
+
+;List of previously compiled sprites (actually entities at the moment...)
+(define compiled '())
+(define csd       #f)  ;Mode Lambda's representation of our compiled sprites
+(define ml:render #f)  ;Graphics card render function
+
+(define (setup-already-compiled-list entities)
+  entities)
+
+(define (find-uncompiled-things game previously-compiled)
+  (if (not game)
+      '()
+      (filter-not (curryr member previously-compiled entity-eq? #;sprite-eq?)
+                  (game-entities game))))
+
+(define (recompile uncompiled W H)
+  (let ([sd2 (ml:make-sprite-db)])
+    (for ([sprite-entity (in-list (append compiled uncompiled))])
+      (and (get-component sprite-entity animated-sprite?)
+           (add-entity! sd2 sprite-entity)))
+    (set! csd (ml:compile-sprite-db sd2))
+    ;(ml:save-csd! csd (build-path "/Users/thoughtstem/Desktop/sprite-db") #:debug? #t)
+    (set! compiled (append compiled uncompiled))
+    (displayln (ml:compiled-sprite-db-spr->idx csd))
+    (set! ml:render (gl:stage-draw/dc csd W H 8))))
+
+(define (game->mode-lambda-sprite-list game)
+  (flatten
+           (filter identity
+                   (for/list ([e (in-list (reverse (game-entities game)))])
+                     (define frame-i   (animated-sprite-current-frame (get-component e animated-sprite?)))
+                     (define id-sym    (id->symbol #:prefix (~a "sprite" frame-i) (get-id e)))
+                     (define sprite-id (ml:sprite-idx csd id-sym))
+
+                    ; (displayln (~a (get-name e) " id: " id-sym " sprite-id: " sprite-id))
+
+                     (if (not sprite-id)
+                           #f
+                           (ml:sprite #:layer 0
+                                      (real->double-flonum (x e))
+                                      (real->double-flonum (y e))
+                                      sprite-id))))))
