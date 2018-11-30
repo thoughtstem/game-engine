b9524d9399a8ee40ee0ae9ce7535ae8eb90b7c13
srfoster <stephen@thoughtstem.com>
2018-11-30 12:07:23 -0800

    storage

---
+++ components/storage.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(provide (struct-out storage)
+         get-storage
+         get-storage-data)
+
+(require "../game-entities.rkt")
+
+
+(struct storage (name data))
+
+(define (storage-with-name? n)
+  (lambda (s)
+    (and (storage? s)
+         (string=? n (storage-name s)))))
+
+(define (get-storage name e)
+  (get-component e (storage-with-name? name) ))
+
+(define (get-storage-data name e)
+  (storage-data
+   (get-storage name e)))
