9992b5e22516e82dfc57d53121ec5ad64d173900
aBlender <JasonLongLe@gmail.com>
2019-03-22 13:36:39 -0700

    Revert "Disabling sound"
    This reverts commit f1fa71744275572d8dd7e14321d413be0a7bca60.

--- components/sound-stream.rkt
+++ components/sound-stream.rkt
@@ -2,7 +2,7 @@
 
 (require "../game-entities.rkt")
 
-#;(require (only-in rsound
+(require (only-in rsound
                   default-sample-rate
                   resample-to-rate
                   rs-read
@@ -21,23 +21,19 @@
          stop-sound-streams)
 
 (displayln "=== CLEARING SOUND THREADS ===")
-;(stop) ;shutting down any loose sound threads at least until a try-catch is made.
+(stop) ;shutting down any loose sound threads at least until a try-catch is made.
 
-;(default-sample-rate 48000)
+(default-sample-rate 48000)
 
 (define (make-sound string-path)
-  '()
-  ;(resample-to-rate 48000 (rs-read (string->path string-path)))
-  )
+  (resample-to-rate 48000 (rs-read (string->path string-path))))
 
 (component sound-stream (ps))
 
 (define (make-sound-stream)
   (with-handlers ([exn:fail? (thunk* (displayln "Error creating sound stream"))])
                  (new-sound-stream
-                   '()
-                   ;(make-pstream)
-                   )))
+                   (make-pstream))))
 
 (define (update-sound-stream g e c) e)
 
@@ -54,7 +50,7 @@
                    (if (and rs
                             (get-component e sound-stream?))
                      (begin
-                       ;(pstream-play (get-sound-stream e) rs)
+                       (pstream-play (get-sound-stream e) rs)
                        e)
                      (begin
                        ;(displayln "WARNING: Missing sound-stream component. Sound will not play.")
@@ -69,13 +65,12 @@
 
 (define (stop-all-sounds)
   (lambda (g e)
-    ;(stop)
+    (stop)
     e))
 
 (define (stop-sound-streams)
-  '()
-  ;(stop)
-  )
+  ;'()
+  (stop))
 
 (new-component sound-stream?
                update-sound-stream) 
