d2806bc4ff9da91c78238cc7b19ccc8c0c8ad29d
aBlender <JasonLongLe@gmail.com>
2020-08-10 11:42:13 -0700

    Removed ? from on-key and on-mouse field names

--- components/on-key.rkt
+++ components/on-key.rkt
@@ -5,18 +5,19 @@
 
 (provide (except-out (struct-out on-key) on-key)
          (rename-out (make-on-key on-key)
-                     (on-key       struct-on-key)
-                     (on-key-rule? struct-on-key-rule)
-                     (on-key-f     struct-on-key-f)))
+                     (on-key      struct-on-key)
+                     (on-key-rule struct-on-key-rule)
+                     (on-key-f    struct-on-key-f)
+                     ))
 
-(component on-key (key rule? f))
+(component on-key (key rule f))
 
-(define (make-on-key key #:rule [rule? (lambda (g e) #t)] f)
-  (new-on-key key rule? f))
+(define (make-on-key key #:rule [rule (lambda (g e) #t)] f)
+  (new-on-key key rule f))
 
 (define (update-on-key g e c)
  (if (and (button-change-down? (on-key-key c) g)
-          ((on-key-rule? c) g e))
+          ((on-key-rule c) g e))
      ((on-key-f c) g e)
      e))
 
